06757a16fbee53b0ec328ff61240ab6a351c1808
Mariko Janes <Marikojanes@gmail.com>
2022-06-01 10:31:20 -0600

    added CRUD

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,44 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
-  end
 
   # Categories CRUD
+  get "/categories" do
+    Category.all.to_json
+  end
 
 
   # Recipes CRUD
   get "/recipes" do
     Recipe.all.to_json
+  end
+
+  post "/recipes" do
+    new_recipe = Recipe.create(
+      category_id: params[:category_id],
+      name: params[:name],
+      ingredients: params[:ingredients],
+      instructions: params[:instructions],
+      image_url: params[:image_url]
+    )
+    new_recipe.to_json
+  end
+
+  patch "/recipes/:id" do
+    recipe = Recipe.find(params[:id])
+    recipe.update(
+      category_id: params[:category_id],
+      name: params[:name],
+      ingredients: params[:ingredients],
+      instructions: params[:instructions],
+      image_url: params[:image_url]
+    )
+    recipe.to_json
+  end
+
+  delete "/recipes/:id" do
+    recipe = Recipe.find(params[:id])
+    recipe.destroy
+    recipe.to_json
   end
 
 end
